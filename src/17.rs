871172fddc4a37ddb902872ab9ab71e82464f479
shaweviggiano903 <shaweviggiano903@gmail.com>
2025-03-22 18:06:18 +0000

    Add 17.rs

---
+++ src/17.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Example Rust code
+    let x = 5;
+    if x > 10 {
+        println!("x is greater than 10");
+    } else if x < 5 {
+        println!("x is less than 5");
+    }
+}
