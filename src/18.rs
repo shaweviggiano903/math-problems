f3fc8ac1cc67c8e756b3420a273661c30664903f
shaweviggiano903 <shaweviggiano903@gmail.com>
2025-03-23 17:50:48 +0000

    Add 18.rs

---
+++ src/18.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Define variables
+    let x = 5.0;
+    let y = 3.0;
+
+    // Perform calculations
+    let result = x + y;
+
+    // Print results
+    println!("The sum of {} and {} is {}", x, y, result);
+}
