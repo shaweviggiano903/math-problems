a81b607a7d85a18f668a0b6017c64f23a9b1e42e
shaweviggiano903 <shaweviggiano903@gmail.com>
2025-04-05 05:39:32 +0000

    Add 25.rs

---
+++ src/25.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 5;
+    let y = 10;
+    
+    // Addition
+    let result = x + y;
+    println!("The sum is: {}", result);
+    
+    // Multiplication
+    let multiplication_result = x * y;
+    println!("The product is: {}", multiplication_result);
+}
