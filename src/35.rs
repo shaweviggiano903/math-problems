02bc0320dc93de9642ec5d5435e8a85ae79d06e1
shaweviggiano903 <shaweviggiano903@gmail.com>
2025-05-18 04:34:29 +0000

    Add 35.rs

---
+++ src/35.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Example Rust code to solve a simple linear equation: 2x + 3 = 7
+
+    let x = (7 - 3) / 2;
+
+    println!("The solution for the equation 2x + 3 = 7 is: {}", x);
+}
